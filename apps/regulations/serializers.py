b7ee3722800707629012505e03671518528d032c
lifelonglearner127 <life.long.learner127@outlook.com>
2020-01-14 17:44:55 +0900

    update attendance time

--- apps/regulations/serializers.py
+++ apps/regulations/serializers.py
@@ -21,6 +21,13 @@ class AttendancePlaceSerializer(serializers.ModelSerializer):
 
 class TimeSlotSerializer(serializers.ModelSerializer):
 
+    open_time = serializers.TimeField(format="%H:%M")
+    start_open_time = serializers.TimeField(format="%H:%M")
+    finish_open_time = serializers.TimeField(format="%H:%M")
+    close_time = serializers.TimeField(format="%H:%M")
+    start_close_time = serializers.TimeField(format="%H:%M")
+    finish_close_time = serializers.TimeField(format="%H:%M")
+
     class Meta:
         model = m.TimeSlot
         exclude = (
@@ -68,3 +75,22 @@ class AttendanceTimeSerializer(serializers.ModelSerializer):
             instance.slots.add(obj)
 
         return instance
+
+
+class AttendanceTimeExportSerializer(serializers.ModelSerializer):
+
+    slots = serializers.SerializerMethodField()
+
+    class Meta:
+        model = m.AttendanceTime
+        fields = (
+            'name', 'slots'
+        )
+
+    def get_slots(self, instance):
+        ret = []
+        for slot in instance.slots.all():
+            ret.append(slot.start_open_time.strftime('%H:%M') + '-' + slot.finish_open_time.strftime('%H:%M'))
+            ret.append(slot.start_close_time.strftime('%H:%M') + '-' + slot.finish_close_time.strftime('%H:%M'))
+
+        return ', '.join(ret)
--- apps/regulations/views.py
+++ apps/regulations/views.py
@@ -1,9 +1,13 @@
+from django.db.models import Q
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from drf_renderer_xlsx.mixins import XLSXFileMixin
+from drf_renderer_xlsx.renderers import XLSXRenderer
 
 from . import models as m
 from . import serializers as s
+from ..core.export import EXCEL_BODY_STYLE, EXCEL_HEAD_STYLE
 
 
 class AttendancePlaceViewSet(viewsets.ModelViewSet):
@@ -26,14 +30,35 @@ class AttendancePlaceViewSet(viewsets.ModelViewSet):
         )
 
 
-class AttendanceTimeViewSet(viewsets.ModelViewSet):
+class AttendanceTimeViewSet(XLSXFileMixin, viewsets.ModelViewSet):
 
     queryset = m.AttendanceTime.objects.all()
     serializer_class = s.AttendanceTimeSerializer
+    body = EXCEL_BODY_STYLE
+
+    def get_column_header(self):
+        ret = EXCEL_HEAD_STYLE
+        ret['titles'] = [
+            '部门', '考勤时间',
+
+        ]
+        return ret
+
+    def get_queryset(self):
+        queryset = self.queryset
+        query_str = self.request.query_params.get('q', None)
+        if query_str:
+            queryset = queryset.filter(Q(name__icontains=query_str) | Q(description__icontains=query_str))
+
+        sort_str = self.request.query_params.get('sort', None)
+        if sort_str:
+            queryset = queryset.order_by(sort_str)
+
+        return queryset
 
     def create(self, request):
         context = {
-            'slots': request.data.pop('slots'),
+            'slots': request.data.pop('slots', []),
         }
 
         serializer = self.serializer_class(
@@ -65,6 +90,16 @@ class AttendanceTimeViewSet(viewsets.ModelViewSet):
             status=status.HTTP_200_OK
         )
 
+    @action(detail=False, methods=['post'], url_path="bulk-delete")
+    def bulk_delete(slef, request):
+        m.AttendanceTime.objects.filter(id__in=request.data.get('ids', [])).delete()
+        return Response(
+            {
+                'msg': 'Delete successfully'
+            },
+            status=status.HTTP_200_OK
+        )
+
     @action(detail=False, url_path="all")
     def get_all_times(self, request):
         return Response(
@@ -76,5 +111,13 @@ class AttendanceTimeViewSet(viewsets.ModelViewSet):
     def get_all_times_names(self, request):
         return Response(
             s.AttendanceTimeNameSerializer(self.queryset, many=True).data,
+            status=status.HTTP_200_OK
+        )
+
+    @action(detail=False, url_path="export", renderer_classes=[XLSXRenderer])
+    def export(self, request):
+        queryset = self.get_queryset()
+        return Response(
+            s.AttendanceTimeExportSerializer(queryset, many=True).data,
             status=status.HTTP_200_OK
         )
