58708288b1a3d8960809436303a249fe4839dba5
lifelonglearner127 <life.long.learner127@outlook.com>
2020-04-04 11:20:26 +0900

    update regulation result serializer

--- apps/regulations/serializers.py
+++ apps/regulations/serializers.py
@@ -468,6 +468,12 @@ class AttendSerializer(serializers.ModelSerializer):
 
         return data
 
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret.pop('image', None)
+        ret["place"] = instance.membership.rule.attendance_place.address
+        return ret
+
 
 class AttendanceDailyReportSerializer(serializers.Serializer):
 
